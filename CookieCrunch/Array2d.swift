af59d3659e0df902c069eeea6a296c723cfefcf3
Doug Trance <dtrance@dtcc.edu>
2016-12-25 16:29:23 -0500

    Candy Crush style game

--- CookieCrunch/Array2d.swift
+++ CookieCrunch/Array2d.swift
@@ -29,25 +29,3 @@ struct Array2d<T> {
 
 
 
-//struct Array2d<T> {
-//    
-//    let columns: Int
-//    let rows: Int
-//    fileprivate var array: Array<T?>
-//    
-//    init(columns: Int, rows: Int) {
-//        self.columns = columns
-//        self.rows = rows
-//        array = Array<T?>(repeating: nil, count: rows*columns)
-//    }
-//    
-//    subscript(column: Int, row: Int) -> T? {
-//        get {
-//            return array[row*columns + column]
-//        }
-//        set {
-//            array[row*columns + column] = newValue
-//        }
-//    }
-//    
-//}
